ee16fbc80f88ffce6208ed17bfb8907559221a3c
danilamdev <dalmabarri@gmail.com>
2023-06-25 23:27:28 -0300

    agregado api resend para email

--- src/worker.js
+++ src/worker.js
@@ -1,31 +1,58 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// export default {
+// 	async fetch(request, env, ctx) {
+// 		const corsHeaders = {
+//       "Access-Control-Allow-Origin": "*",
+//       "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+// 			"Content-Type": 'application/json; multipart/form-data'
+//     };
+
+// 		console.log('method is; ', request.method)
+// 		console.log('headers is:', request.headers.get('Content-Type'))
+// 		// const formdata = await request.formData()
+		
+// 		// const data = Object.fromEntries(formdata)
+// 		// const json = JSON.stringify(data)
+
+// 		// const response = new Response(json, {headers: corsHeaders})
+
+// 		console.log(env.MY_API)
+// 		console.log(env.API_KEY)
+
+// 		const  response = new Response(JSON.stringify({status: 'ok'}))
+
+// 		return response
+// 	},
+// };
+
 
 export default {
-	async fetch(request, env, ctx) {
+  async fetch(request, env, ctx) {
 		const corsHeaders = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
 			"Content-Type": 'application/json; multipart/form-data'
-    };
-
-		console.log('method is; ', request.method)
-		console.log('headers is:', request.headers.get('Content-Type'))
+		};
+   
 		const formdata = await request.formData()
-		
-		const data = Object.fromEntries(formdata)
-		const json = JSON.stringify(data)
+		const {nombre, email, mensaje, html} = Object.fromEntries(formdata)
 
-		const response = new Response(json, {headers: corsHeaders})
+    const response = await fetch('https://api.resend.com/emails', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${env.RESEND_API_KEY}`
+      },
+      body: JSON.stringify({
+        from: 'Cursos de Extension <onboarding@resend.dev>',
+        to: env.TO_EMAIL,
+        subject: `Nuevo mensaje de ${email}`,
+        html
+      })
+    });
+    
+    const json = await response.json()
+		console.log('json', json)
 
-
-		return response
-	},
+		return new Response(JSON.stringify(json), {headers: corsHeaders})
+  },
 };

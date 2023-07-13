48fd6cb5d4ce3d2e124146c72aa0efb24f48bfd3
danilamdev <dalmabarri@gmail.com>
2023-07-13 12:44:59 -0300

    fix formdata

--- src/worker.js
+++ src/worker.js
@@ -1,30 +1,3 @@
-// export default {
-// 	async fetch(request, env, ctx) {
-// 		const corsHeaders = {
-//       "Access-Control-Allow-Origin": "*",
-//       "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-// 			"Content-Type": 'application/json; multipart/form-data'
-//     };
-
-// 		console.log('method is; ', request.method)
-// 		console.log('headers is:', request.headers.get('Content-Type'))
-// 		// const formdata = await request.formData()
-		
-// 		// const data = Object.fromEntries(formdata)
-// 		// const json = JSON.stringify(data)
-
-// 		// const response = new Response(json, {headers: corsHeaders})
-
-// 		console.log(env.MY_API)
-// 		console.log(env.API_KEY)
-
-// 		const  response = new Response(JSON.stringify({status: 'ok'}))
-
-// 		return response
-// 	},
-// };
-
-
 export default {
   async fetch(request, env, ctx) {
 		const corsHeaders = {
@@ -34,7 +7,7 @@ export default {
 		};
    
 		const formdata = await request.formData()
-		const {nombre, email, mensaje, html} = Object.fromEntries(formdata)
+		const { email, html} = Object.fromEntries(formdata)
 
     const response = await fetch('https://api.resend.com/emails', {
       method: 'POST',
@@ -43,7 +16,7 @@ export default {
         'Authorization': `Bearer ${env.RESEND_API_KEY}`
       },
       body: JSON.stringify({
-        from: 'Cursos de Extension <onboarding@resend.dev>',
+        from: '<<no-replay>@cursosextensionuniversitarios.com>',
         to: env.TO_EMAIL,
         subject: `Nuevo mensaje de ${email}`,
         html

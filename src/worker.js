08fc666b62c6af6734b32905cecab513ea91260c
danilamdev <dalmabarri@gmail.com>
2023-07-13 13:17:27 -0300

    fix email send

--- src/worker.js
+++ src/worker.js
@@ -1,13 +1,13 @@
 export default {
   async fetch(request, env, ctx) {
-		const corsHeaders = {
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-			"Content-Type": 'application/json; multipart/form-data'
-		};
-   
-		const formdata = await request.formData()
-		const { email, html} = Object.fromEntries(formdata)
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+      "Content-Type": 'application/json; multipart/form-data'
+    };
+
+    const formdata = await request.formData()
+    const { email, html } = Object.fromEntries(formdata)
 
     const response = await fetch('https://api.resend.com/emails', {
       method: 'POST',
@@ -16,16 +16,16 @@ export default {
         'Authorization': `Bearer ${env.RESEND_API_KEY}`
       },
       body: JSON.stringify({
-        from: '<<no-replay>@cursosextensionuniversitarios.com>',
+        from: '<no-replay@cursosextensionuniversitarios.com>',
         to: env.TO_EMAIL,
         subject: `Nuevo mensaje de ${email}`,
         html
       })
     });
-    
-    const json = await response.json()
-		console.log('json', json)
 
-		return new Response(JSON.stringify(json), {headers: corsHeaders})
+    const json = await response.json()
+    console.log('json', json)
+
+    return new Response(JSON.stringify(json), { headers: corsHeaders })
   },
 };
